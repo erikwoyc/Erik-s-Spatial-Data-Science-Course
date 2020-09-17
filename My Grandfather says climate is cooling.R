0995f38fd26e755c0eb891ad1391d76ca44f1da9
erikwoyc <erikwoyc@buffalo.edu>
2020-09-16 20:39:00 -0400

    Second commit. Needed to change url.

--- My Grandfather says climate is cooling.R
+++ My Grandfather says climate is cooling.R
@@ -1,7 +1,8 @@
 library(tidyverse)
 library(ggplot2)
 library(ggthemes)
-dataurl="https://data.giss.nasa.gov/tmp/gistemp/STATIONS/tmp_USW00014733_14_0_1/station.csv"
+#Was having trouble with url so I changed it to the second one provided
+dataurl="https://raw.githubusercontent.com/AdamWilsonLab/GEO511/master/CS_02.csv"
 temp=read_csv(dataurl,
               skip=1, #skip the first line which has column names
               na="999.90", # tell R that 999.90 means missing in this dataset
@@ -22,4 +23,3 @@ ggplot(Summer_temps, aes(YEAR,Means)) +
        caption = ~ atop(paste("Data from the Global Historical Climate Network"),
                         paste("Red Line is a LOESS Line"))) +
   theme_economist()
-       
